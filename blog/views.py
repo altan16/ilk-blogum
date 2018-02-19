98291fc43dbe10602dcca0335bf0cee203411c6f
Emre Altan <altan16@itu.edu.tr>
2018-02-19 22:58:33 +0300

    Updated

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404,render_to_response
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.http import HttpResponse
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    son_yazilanlar=Post.objects.order_by('-yayinlanma_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts,'son_yazilanlar': son_yazilanlar})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -36,4 +38,6 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
+def login(request):
+	
+	return render_to_response('blog/login.html',locals())
